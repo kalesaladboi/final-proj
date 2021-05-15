84f7c2337933d81458bc9c8b119c1053871fc0e7
kalesaladboi <73973027+kalesaladboi@users.noreply.github.com>
2021-05-15 11:58:38 -0400

    init db/server commit

---
+++ server/routes/routes.js
@@ -0,0 +1,22 @@
+const { response } = require('express')
+const express = require('express')
+const router = express.Router()
+const register = require('../modles/register')
+
+router.post('/register', (req,res) => {
+    const registerUser = new register({
+        name:req.body.name,
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+    })
+    registerUser.save()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(error => {
+        res.json(error)
+    })
+})
+
+module.exports = router
