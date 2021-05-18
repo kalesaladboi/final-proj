b239f03721f14e74d5cf65dab59925a302357f47
Silverlin19 <silverline1996@gmail.com>
2021-05-18 17:22:53 -0400

    app is now woke

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,24 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-workbox.routing.registerRoute(
-    ({request}) => request.destination === "image",
-    new workbox.strategies.NetworkFirst()
-)
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === "image",
+//     new workbox.strategies.NetworkFirst()
+// )
+
+
+self.addEventListener('install', e=> {
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll(["./","./src/main.css" ,"./icons/apple-splash-1170-2532.jpg"]);
+
+        })
+    );
+});
+
+self.addEventListener("fetch", e=> {
+e.respondWith(
+    caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+    })
+);    
+});
