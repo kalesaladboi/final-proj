d6613e0d2d87a9617e51d24ca19c5b1f8d2a57a0
kalesaladboi <73973027+kalesaladboi@users.noreply.github.com>
2021-05-26 07:36:30 -0400

    stream

--- client/src/components/MovieNight.js
+++ client/src/components/MovieNight.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import VideoPlayer from 'react-video-js-player'
 import wasabi from "./assets/wasabi.mp4"
+import vossiBop from "./assets/vossibop.mp4"
+import sayless from "./assets/sayless.mp4"
 import '@videojs/themes/dist/fantasy/index.css'
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 
 
 function MovieNight() {
 
-    const videoSrc = wasabi
+    const option = [wasabi, vossiBop, sayless]
+    const [video, setVideo] = useState([])
+    const defaultVideo = video[0]
+    const videoSrc = sayless;
+
+
+    useEffect(() => {
+       setVideo(wasabi)
+    }, [])
     
+    const handleChange = e => {
+        setVideo(e.target.value)
+        .then(console.log(video))
+    }
+
     return (
         <div className="video-player">
             <VideoPlayer
@@ -15,6 +32,7 @@ function MovieNight() {
             width = "720"
             height = "420"
             />
+            <Dropdown options={option} onChange={handleChange} value={defaultVideo} placeholder="Select a Video" />
         </div>
     )
 }
