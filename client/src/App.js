3197e5675e2115b7550e9d12ebf4b55b32b6f463
Silverlin19 <silverline1996@gmail.com>
2021-05-27 12:40:41 -0400

    Try again and again

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-=======
-import React, { Component } from "react"
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import axios from 'axios'
-import Dashboard from "./components/Dashboard"
-import Home from "./components/Home"
-import MovieNight from "./components/MovieNight"
-import Header from "./components/Header"
-import Sidebar from "./components/Sidebar"
-import './App.css'
->>>>>>> 1ee8a78abe5311fa73a6993ac587b96c10748fc8
 
 import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
@@ -38,95 +26,4 @@ const App = () => {
   );
 };
 
-<<<<<<< HEAD
 export default App;
-=======
-  
-
-  checkLoginStatus() {
-    axios.get("http://localhost:3001/logged_in", { withCredentials: true})
-    .then(response => {
-      console.log("logged in?", response)
-    }).catch(error =>{
-      console.log("login error:", error)
-    })
-  }
-
-  componentDidMount() {
-    this.checkLoginStatus()
-  }
-
-  handleLogin(data) {
-    this.setState({
-      loggedInStatus: "LOGGED_IN",
-      user: data.user
-    })
-  }
-  
-  
-  render(){
-    const items = [
-      { name: 'home', label: 'Home' },
-      {
-        name: 'billing',
-        label: 'Billing',
-        items: [
-          { name: 'statements', label: 'Statements' },
-          { name: 'reports', label: 'Reports' },
-        ],
-      },
-      {
-        name: 'settings',
-        label: 'Settings',
-        items: [
-          { name: 'profile', label: 'Profile' },
-          {
-            name: 'notifications',
-            label: 'Notifications',
-            items: [
-              { name: 'email', label: 'Email' },
-              { name: 'sms', label: 'SMS' },
-            ],
-          },
-        ],
-      },
-    ]
-    
-    return (
-      <div className='app'>
-        <Header />
-        <div>
-          <Sidebar items={items} />
-        </div>
-          <BrowserRouter>
-            <Switch>
-              <Route 
-              exact 
-              path={"/"} 
-              render={props => (
-                <Home {... props} handleLogin={this.handleLogin} loggedInStatus={this.state.loggedInStatus} />
-            )}
-            />
-            <Route 
-            exact 
-            path={"/dashboard"} 
-            render={props => (
-              <Dashboard {... props} loggedInStatus={this.state.loggedInStatus} />
-            )}
-            />
-            <Route 
-            exact 
-            path={"/movie"} 
-            render={props => (
-              <MovieNight {... props} loggedInStatus={this.state.loggedInStatus} />
-            )}
-            />
-            <Route />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    )
-  }
-}
-
->>>>>>> 1ee8a78abe5311fa73a6993ac587b96c10748fc8
--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,9 @@ import thunk from 'redux-thunk';
 
 import { reducers } from './reducers';
 import App from './App';
-<<<<<<< HEAD
 import './index.css';
 
 const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
-=======
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
->>>>>>> 1ee8a78abe5311fa73a6993ac587b96c10748fc8
 
 ReactDOM.render(
   <Provider store={store}>
