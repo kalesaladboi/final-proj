bc5ae80c039b458232b170c3b94f646fac17a6f6
kalesaladboi <73973027+kalesaladboi@users.noreply.github.com>
2021-06-01 17:38:49 -0400

    added video

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
-import Movie from './components/Movie/Movie';
+import MovieNight from './components/Movie/movie';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
@@ -20,7 +20,7 @@ const App = () => {
           <Route path="/posts" exact component={Home} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" exact component={PostDetails} />
-          <Route path="/Movie" exact component={Movie} />
+          <Route path="/Movie" exact component={MovieNight} />
           <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
         </Switch>
       </Container>
