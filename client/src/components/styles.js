c2ebfd1937f63d904ab14a117924771f5f532ec5
Silverlin19 <silverline1996@gmail.com>
2021-06-01 19:08:56 -0400

    press

--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -3,5 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(() => ({
   ul: {
     justifyContent: 'space-around',
+    position:'absolute',
+   bottom:'0',
+   width:'100%',
+   height:'60px',
   },
 }));
