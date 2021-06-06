845d6846b45317eae6c564e61cc2a31afc66839d
Emma Gollan <emmagollan33@gmail.com>
2021-06-06 12:36:17 -0400

    changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -36,32 +36,37 @@ const Navbar = () => {
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
 
-  return (
-    <AppBar className={classes.appBar} position="static" color="default">
-      <div className = "logos">
-      <Link to="/" className={classes.brandContainer}>
-        <img className={classes.logo} src={sociallogo} alt="icon" height="100px" />
-      </Link>
-      <Link to="Movie/movie" className={classes.brandContainer}>
-        <img className={classes.logo} src={MNLogo} alt="icon" height="100px" />
-      </Link>
-      </div>
-      <Typography color="textPrimary" variant="h2">SOCIAL</Typography>
-      <Toolbar className={classes.toolbar}>
-        {user?.result ? (
-          <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="primary" onClick={logout}>Logout</Button>
-          </div>
-        ) : (
-          <>
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-          </>
-        )}
-      </Toolbar>
-    </AppBar>
-  );
+
+return (
+  <AppBar className={classes.appBar} position="static" color="default">
+    <div className = "logos" >
+    <Link to="/" className={classes.brandContainer}>
+      <img className={classes.logo} src={sociallogo} alt="icon" height="90px" />
+    </Link>
+    <Link to="/movie" className={classes.brandContainer}>
+      <img className={classes.logo} src={MNLogo} alt="icon" height="90px" />
+    </Link>
+    </div>
+    <Typography color="textPrimary" variant="h2">SOCIAL</Typography>
+    <Toolbar className={classes.toolbar}>
+      {user?.result ? (
+        <div className={classes.profile}>
+          <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+          <Typography className={classes.userName} variant="h5">{user?.result.name}</Typography>
+          <Button variant="contained" className={classes.logout} color="primary" onClick={logout}>Logout</Button>
+        </div>
+      ) : (
+        <>
+        <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+        </>
+      )}
+    </Toolbar>
+  </AppBar>
+);
 };
+
+
+
+
 
 export default Navbar;
