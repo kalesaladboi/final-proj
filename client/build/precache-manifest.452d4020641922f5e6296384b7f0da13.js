f96bed28dd742719393c008e5cb4697d07a678e7
kalesaladboi <73973027+kalesaladboi@users.noreply.github.com>
2021-06-19 22:59:53 -0400

    build

---
+++ client/build/precache-manifest.452d4020641922f5e6296384b7f0da13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed078a7dc6ba168ec14de6776b1471f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a2e6e4a582bb296c99c",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "2c7a237340f3c4eda592",
+    "url": "/static/css/main.1ff7d3a4.chunk.css"
+  },
+  {
+    "revision": "3a2e6e4a582bb296c99c",
+    "url": "/static/js/2.5d2ac64b.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.5d2ac64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7a237340f3c4eda592",
+    "url": "/static/js/main.71f4cec1.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "6d8cfa89b687566b71f03ca69827abe6",
+    "url": "/static/media/MNLogo.6d8cfa89.png"
+  },
+  {
+    "revision": "278f598488486fdf49d4621cfea72294",
+    "url": "/static/media/sociallogo.278f5984.png"
+  }
+]);
